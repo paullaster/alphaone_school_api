d934b1939eab243ce1b62a3ee5c6ff29f91ba7ad
paullaster <paullasterokoth98@gmail.com>
2024-02-10 19:08:09 +0300

    scafold alphaone api backend

--- src/app/http/controllers/users/Auth/Authcontroller.js
+++ src/app/http/controllers/users/Auth/Authcontroller.js
@@ -7,25 +7,30 @@ class AuthController {
 
     };
     async signup(req, res) {
-        const url = `${application.weburl}/getstarted/confirm/${new Buffer(req.body.email).toString('base64')}`
+        try {
+          const url = `${application.weburl}/getstarted/confirm/${new Buffer(req.body.email).toString('base64')}`
         const mailSubject = "Email account verification";
         const mailBody = `
-    <p style="font-family: sans-serif; font-size: 16px; line-height: 1.5; margin: 0 0 20px; color: #333">
-    Hello, ${req.body.email}
-  </p>
-  <p style="font-family: sans-serif; font-size: 16px; line-height: 1.5; margin: 0 0 20px; color: #333">
-    Please confirm your email address to unlock exclusive features and benefits.
-  </p>
-  <p style="margin: 0">
-    <a href="${url}" style="background-color: #007bff; border: none; border-radius: 5px; color: #fff; display: inline-block; font-family: sans-serif; font-size: 15px; font-weight: bold; line-height: 40px; padding: 10px 25px; text-align: center; text-decoration: none; vertical-align: middle;">Confirm Email Address</a>
-  </p>
-  <p style="font-family: sans-serif; font-size: 12px; color: #999; margin-top: 20px">
-    You can also copy and paste this link into a new browser tab: <a href="${url}" style="color: #999; text-decoration: none;">${url}</a>
-  </p>
+                  <p style="font-family: sans-serif; font-size: 16px; line-height: 1.5; margin: 0 0 20px; color: #333">
+                  Hello, ${req.body.email}
+                </p>
+                <p style="font-family: sans-serif; font-size: 16px; line-height: 1.5; margin: 0 0 20px; color: #333">
+                  Please confirm your email address to unlock exclusive features and benefits.
+                </p>
+                <p style="margin: 0">
+                  <a href="${url}" style="background-color: #007bff; border: none; border-radius: 5px; color: #fff; display: inline-block; font-family: sans-serif; font-size: 15px; font-weight: bold; line-height: 40px; padding: 10px 25px; text-align: center; text-decoration: none; vertical-align: middle;">Confirm Email Address</a>
+                </p>
+                <p style="font-family: sans-serif; font-size: 12px; color: #999; margin-top: 20px">
+                  You can also copy and paste this link into a new browser tab: <a href="${url}" style="color: #999; text-decoration: none;">${url}</a>
+                </p>
     `;
         const notify = new EmailVerificationNotification(req.body.email, mailSubject, mailBody);
         const mail = await notify.via('viaEmail');
-        res.send(mail);
+        res.sendStatus(mail).json({});
+        } catch (error) {
+          
+        }
+        
 
     }
 };
---
+++ src/app/http/middlewares/ApiResponder.js
@@ -0,0 +1,17 @@
+class ApiResponder {
+    res = null;
+    constructor(res) {
+        this.res = res;
+    }
+    success(data = {}, statusCode = 200, message = 'Success') {
+try {
+    return this.res.sendStatus(statusCode).json({data, message});
+} catch (error) {
+        return this.error(error, error.message, 500);
+}    }
+    error(data = {}, message = "Error", statusCode = 500 ) {
+        return this.res.sendStatus(statusCode).json({data, message});
+    }
+}
+
+export default new ApiResponder();
