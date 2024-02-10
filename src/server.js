36ed51095d5e1b5305f400256ac15025360464f7
paullaster <paullasterokoth98@gmail.com>
2024-02-10 09:34:40 +0300

    scafold alphaone api backend

--- src/server.js
+++ src/server.js
@@ -10,10 +10,6 @@ const app = express();
 app.use(route)
 
 
-app.get('/', (req, res) => {
-    res.send("I am back home after a long time");
-});
-
 
 app.listen(application.port, () => {
     console.log(`Server started. ${application.url}`)
