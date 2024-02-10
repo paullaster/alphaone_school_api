dd79dd64b611bb8e94b2477e16369215ae2101c9
paullaster <paullasterokoth98@gmail.com>
2024-02-10 12:49:30 +0300

    scafold alphaone api backend

--- src/app/http/controllers/users/Auth/Authcontroller.js
+++ src/app/http/controllers/users/Auth/Authcontroller.js
@@ -16,10 +16,10 @@ class AuthController {
     Please confirm your email address to unlock exclusive features and benefits.
   </p>
   <p style="margin: 0">
-    <a href="${application.weburl}getstarted/confirm" style="background-color: #007bff; border: none; border-radius: 5px; color: #fff; display: inline-block; font-family: sans-serif; font-size: 15px; font-weight: bold; line-height: 40px; padding: 10px 25px; text-align: center; text-decoration: none; vertical-align: middle;">Confirm Email Address</a>
+    <a href="${application.weburl}/getstarted/confirm" style="background-color: #007bff; border: none; border-radius: 5px; color: #fff; display: inline-block; font-family: sans-serif; font-size: 15px; font-weight: bold; line-height: 40px; padding: 10px 25px; text-align: center; text-decoration: none; vertical-align: middle;">Confirm Email Address</a>
   </p>
   <p style="font-family: sans-serif; font-size: 12px; color: #999; margin-top: 20px">
-    You can also copy and paste this link into a new browser tab: <a href="${application.weburl}" style="color: #999; text-decoration: none;">${application.weburl}getstarted/confirm</a>
+    You can also copy and paste this link into a new browser tab: <a href="${application.weburl}/getstarted/confirm" style="color: #999; text-decoration: none;">${application.weburl}/getstarted/confirm</a>
   </p>
   
     `;
