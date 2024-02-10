fbeac6923ea346f8fc2fe3c0f36b145835be418a
paullaster <paullasterokoth98@gmail.com>
2024-02-10 13:10:03 +0300

    scafold alphaone api backend

--- src/app/http/controllers/users/Auth/Authcontroller.js
+++ src/app/http/controllers/users/Auth/Authcontroller.js
@@ -7,7 +7,7 @@ class AuthController {
 
     };
     async signup(req, res) {
-        const url = `${application.weburl}/getstarted/confirm/${atob(req.body.email)}`
+        const url = `${application.weburl}/getstarted/confirm/${new Buffer(req.body.email).toString('base64')}`
         const mailSubject = "Email account verification";
         const mailBody = `
     <p style="font-family: sans-serif; font-size: 16px; line-height: 1.5; margin: 0 0 20px; color: #333">
@@ -20,7 +20,7 @@ class AuthController {
     <a href="${url}" style="background-color: #007bff; border: none; border-radius: 5px; color: #fff; display: inline-block; font-family: sans-serif; font-size: 15px; font-weight: bold; line-height: 40px; padding: 10px 25px; text-align: center; text-decoration: none; vertical-align: middle;">Confirm Email Address</a>
   </p>
   <p style="font-family: sans-serif; font-size: 12px; color: #999; margin-top: 20px">
-    You can also copy and paste this link into a new browser tab: <a href="${url}/getstarted/confirm" style="color: #999; text-decoration: none;">${url}/getstarted/confirm</a>
+    You can also copy and paste this link into a new browser tab: <a href="${url}" style="color: #999; text-decoration: none;">${url}</a>
   </p>
     `;
         const notify = new EmailVerificationNotification(req.body.email, mailSubject, mailBody);
