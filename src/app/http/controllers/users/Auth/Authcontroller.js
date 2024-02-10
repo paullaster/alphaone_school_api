4758f0ffd4698ad2947524eef9189eefba7bbbe5
paullaster <paullasterokoth98@gmail.com>
2024-02-10 12:05:03 +0300

    scafold alphaone api backend

--- src/app/http/controllers/users/Auth/Authcontroller.js
+++ src/app/http/controllers/users/Auth/Authcontroller.js
@@ -1,4 +1,5 @@
 import EmailVerificationNotification from "../../../notifications/EmailVerificationNotification.js";
+import { application } from "../../../../../config";
 
 class AuthController {
 
@@ -6,6 +7,13 @@ class AuthController {
 
    };
    async signup(req, res) {
+    const mailSubject = "Email account verification";
+    const mailbody = `
+    <p> Hellor, ${ req.body.email } </p>
+    <p> Please click the link below to confirm your email</p>
+    <p><a href=${application.weburl}>Confirm Email Address </a></p>
+    `
+    const notify = new EmailVerificationNotification(req.body.email, mailSubject, )
    }
 };
 
--- src/config/app.js
+++ src/config/app.js
@@ -2,4 +2,5 @@ export const application = {
     name: process.env.APP_NAME,
     url: process.env.APP_URL,
     port: process.env.APP_PORT,
+    weburl: process.env.WEB_URL
 }
