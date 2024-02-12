8219532effe5b74b844bc4dfcf272e8e7b3f5778
paullaster <paullasterokoth98@gmail.com>
2024-02-12 16:41:26 +0300

    add database connetction

---
+++ src/app/models/User.js
@@ -0,0 +1,39 @@
+import { sequilize } from "../../database/index.js";
+import { DataTypes, Model} from 'sequelize';
+
+class User extends Model {};
+
+User.init({
+    id: {
+        type : DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+    },
+    type: {
+        type: DataTypes.ENUM('leaner', 'admin'),
+        allowNull: false,
+        defaultValue: 'leaner',
+    },
+
+
+}, {
+    sequilize,
+    timestamps: true,
+});
+
+User.sync();
+
+export { User };
