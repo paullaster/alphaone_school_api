99293e1f59c770da10ee80e02e3ef229ead339b6
paullaster <paullasterokoth98@gmail.com>
2024-02-14 07:17:05 +0300

    update error middle and user create

--- src/app/http/controllers/courses/CoursesController.js
+++ src/app/http/controllers/courses/CoursesController.js
@@ -21,11 +21,19 @@ class CoursesController {
             res.ApiResponse.error(error, 'We ran into an error when creating this course!');
         }
     }
-    findCourse(req, res) {
+    async findCourse(req, res) {
         try {
-            
+            const course = await Course.findOne({
+                where: {
+                    id: req.body.courseID,
+                },
+            });
+            if (!course) {
+                res.ApiResponse.error(course, "No record matched this requested course!", 404);
+            }
+            res.ApiResponse.success(course);
         } catch (error) {
-            
+            res.ApiResponse.error(error, 'Error fetching this course');
         }
     }
     updateCourse(req, res) {
