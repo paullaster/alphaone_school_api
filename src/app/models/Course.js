f9faff96950e7596599dd7d207d9f6621d36db48
paullaster <paullasterokoth98@gmail.com>
2024-02-14 06:51:39 +0300

    update error middle and user create

---
+++ src/app/models/Course.js
@@ -0,0 +1,28 @@
+import { sequilize } from "../../database/index.js";
+import { DataTypes } from 'sequelize';
+
+const Course = sequilize.define('Course', {
+    id: {
+        type: DataTypes.STRING,
+        primaryKey: true,
+        unique: true,
+        allowNull: false,
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    price: {
+        type: DataTypes.FLOAT,
+        allowNull: false,
+    },
+    duration: {
+        type: DataTypes.STRING,
+        allowNull: false,
+
+    }
+});
+
+Course.sync();
+
+export { Course };
