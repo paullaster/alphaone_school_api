cbe390cce6f7af78b36ec6eb19976d31090cca7a
paullaster <paullasterokoth98@gmail.com>
2024-02-15 13:46:22 +0300

    update api logic

--- src/routes/applications/application.js
+++ src/routes/applications/application.js
@@ -1,6 +1,8 @@
 import express from 'express';
+import ApplicationController from '../../app/http/controllers/application/ApplicationController';
 const applicationRoutes = express.Router();
 
-
+applicationRoutes.get('/', ApplicationController.applications);
+applicationRoutes.get('/single', ApplicationController.application);
 
 export {  applicationRoutes };
