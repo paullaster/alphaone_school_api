092996cdbe68ed621a04d5c4515e57755e74c7e2
paullaster <paullasterokoth98@gmail.com>
2024-02-15 09:38:19 +0300

    update api logic

--- src/app/models/Application.js
+++ src/app/models/Application.js
@@ -1,10 +1,18 @@
 import { sequilize } from "../../database/index.js";
 import { DataTypes } from 'sequelize';
+import { Course } from "./Course.js";
 
 const Application = sequilize.define('Application', {
     id: {
         type: DataTypes.STRING,
         primaryKey: true,
         unique: true,
+    },
+    course: {
+        type: DataTypes.STRING,
+        references: {
+            model: Course,
+            key: 'id',
+        }
     }
 })
