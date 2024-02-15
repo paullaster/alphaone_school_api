541451ceb9cc122169c68d5b33ffbbb02dcc949c
paullaster <paullasterokoth98@gmail.com>
2024-02-15 10:29:08 +0300

    update api logic

--- src/app/http/applicationController/ApplicationController.js
+++ src/app/http/applicationController/ApplicationController.js
@@ -1,9 +1,12 @@
+import { Application } from "../../models/Application";
+
 class ApplicationController {
-    apply(req, res) {
+    async apply(req, res) {
         try {
-            
+            const application = await Application.create(req.body);
+            res.ApiResponse.success(application, 201, 'Application made successfully');
         } catch (error) {
-            
+            res.ApiResponse.error(error, 'Error creating application');
         }
     }
 }
