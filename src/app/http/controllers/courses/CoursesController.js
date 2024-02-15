bf4082a2cbe5bab39145865f26720ed00fd2b63e
paullaster <paullasterokoth98@gmail.com>
2024-02-15 12:44:22 +0300

    update api logic

--- src/app/http/controllers/courses/CoursesController.js
+++ src/app/http/controllers/courses/CoursesController.js
@@ -44,9 +44,11 @@ class CoursesController {
             if (!course) {
                 res.ApiResponse.error(course, 'We can find this course', 404);
             }
-            for (prop in req.body) {
+            for (let prop in req.body) {
                 course[prop] = req.body[prop];
             }
+            const updatedCourse = await course.save();
+            res.ApiResponse.success(updatedCourse, 201, "Updated successfully");
         } catch (error) {
             
         }
