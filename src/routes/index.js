8cd7f4e33db3d2dc7e9e239b8ea7ed865e5bd061
paullaster <paullasterokoth98@gmail.com>
2024-02-15 13:34:17 +0300

    update api logic

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import { usersRouter } from './users/index.js';
+import { useCoursesRoutes } from './courses/index.js';
 
 const router = express.Router();
 
 
 
 router.use('/users', usersRouter);
-router.use(useCou)
+router.use(useCoursesRoutes);
 // router.use('/admin', );
 export {router};
