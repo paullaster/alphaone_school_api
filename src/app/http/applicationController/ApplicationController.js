2fb1a40e50c62e672dbadf3db450d3ef462fd79c
paullaster <paullasterokoth98@gmail.com>
2024-02-15 11:32:02 +0300

    update api logic

--- src/app/http/applicationController/ApplicationController.js
+++ src/app/http/applicationController/ApplicationController.js
@@ -9,12 +9,30 @@ class ApplicationController {
             res.ApiResponse.error(error, 'Error creating application');
         }
     }
-    async applications (req, res) {
+    async applications(req, res) {
         try {
             const applications = await Application.findAndCountAll({});
             res.ApiResponse.success(applications, 200);
         } catch (error) {
             res.ApiResponse.error(error);
+        }
+    }
+    async update(req, res) {
+        try {
+            const application = await Application.findOne({
+                where: {
+                    id: req.body.applicationID,
+                }
+            });
+            for (let prop in req.body) {
+                if (prop !== 'applicationID') {
+                    application[prop] = req.body[prop];
+                }
+            }
+            const updatedApplication = await application.save();
+            res.ApiResponse.success(updatedApplication, 201, "Application updated");
+        } catch (error) {
+
         }
     }
 }
