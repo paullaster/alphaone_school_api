281cfaedc9cceb8a712aef24c516221de48b5492
paullaster <paullasterokoth98@gmail.com>
2024-02-19 00:32:58 +0300

    update backend

--- src/routes/resources/index.js
+++ src/routes/resources/index.js
@@ -1,7 +1,9 @@
 import express from "express";
 import { imageRoutes } from "./images.js";
+import { setupRoutes } from "./setup.js";
 const useResourceRoutes = express.Router();
 
 useResourceRoutes.use('/images', imageRoutes);
+useResourceRoutes.use('/setup', setupRoutes);
 
 export { useResourceRoutes };
