7d9d88d4002db95bf4a62e77d5fc18c7fc6201b5
paullaster <paullasterokoth98@gmail.com>
2024-02-18 18:45:15 +0300

    update backend

--- src/app/http/controllers/assets/ImagesController.js
+++ src/app/http/controllers/assets/ImagesController.js
@@ -9,6 +9,21 @@ class ImageController {
             res.ApiResponse.error(error);
         }
     }
+    async getImage (req, res) {
+        try {
+            const image = await Image.findOne({
+                wehere: {
+                    sourceID: req.body.recordID,
+                },
+            });
+            if (!image) {
+                res.ApiResponse.error(image, 'resource not found!', 404)
+            }
+            res.ApiResponse.success(image, 200);
+        } catch (error) {
+            res.ApiResponse.error(error);
+        }
+    }
 }
 
 export default new ImageController();
