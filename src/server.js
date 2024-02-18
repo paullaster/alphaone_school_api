155b921c814aa0b346473d748c8314845a6d52a6
paullaster <paullasterokoth98@gmail.com>
2024-02-18 15:32:28 +0300

    update backend

--- src/server.js
+++ src/server.js
@@ -3,6 +3,8 @@ import cors from 'cors';
 import { application } from './config/index.js';
 import {route } from './app/http/providers/index.js';
 import { ApiResponder } from './app/http/middlewares/ApiResponder.js';
+// import { staticFiles } from './app/http/middlewares/ServeStaticFiles.js';
+import path from  'node:path';
 
 
 // APP INSTANCE
@@ -14,9 +16,8 @@ app.use(express.json());
 app.use(cors());
 
 app.use(ApiResponder);
-app.get('/storage', (req,res) => {
- res.send('file');
-});
+app.use('/storage/*', express.static(path.join(__dirname, 'storage', 'public', 'images')));
+// app.use(staticFiles);
 
 app.use(route)
 
