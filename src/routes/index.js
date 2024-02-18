1b53e9312d7a4621d800f4e3c54fd14247e6767f
paullaster <paullasterokoth98@gmail.com>
2024-02-18 18:55:37 +0300

    update backend

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import { usersRouter } from './users/index.js';
 import { useCoursesRoutes } from './courses/index.js';
 import { useApplicationRoutes } from './applications/index.js';
+import { useResourceRoutes } from './resources/index.js';
 
 const router = express.Router();
 
@@ -10,4 +11,5 @@ const router = express.Router();
 router.use('/users', usersRouter);
 router.use(useCoursesRoutes);
 router.use(useApplicationRoutes);
+router.use(useResourceRoutes);
 export {router};
