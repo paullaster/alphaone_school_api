174bd175dd5eb0c6084e7b01f681a83c119eaeb4
paullaster <paullasterokoth98@gmail.com>
2024-02-19 01:15:53 +0300

    update backend

--- src/app/http/controllers/setup/SetupController.js
+++ src/app/http/controllers/setup/SetupController.js
@@ -3,17 +3,6 @@ import countries from '../../../../../countries.json' with { type: "json" };
 class Setupcontroller {
     async getCountriesList(req, res) {
         try {
-            const query = req.query;
-            const countryApi = `https://restcountries.com/v3.1/all`;
-            axios.interceptors.request.use(config => {
-                config.params = {
-                    ...query,
-                }
-                return config;
-            });
-
-            // const response = await axios.get(countryApi);
-            // console.log(response);
             res.ApiResponse.success(countries);
 
         } catch (error) {
