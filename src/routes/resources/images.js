cf3dbb8f19d30653b780f2292a434c49408ed905
paullaster <paullasterokoth98@gmail.com>
2024-02-18 18:52:17 +0300

    update backend

--- src/routes/resources/images.js
+++ src/routes/resources/images.js
@@ -2,3 +2,6 @@ import express from "express";
 import ImagesController from "../../app/http/controllers/assets/ImagesController.js";
 
 const imageRoutes = express.Router();
+
+coursesRoutes.get('/', ImagesController.listCourses);
+coursesRoutes.get('/single', ImagesController.findCourse);
