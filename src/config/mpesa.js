78f828a1cc6904efb8b0522c38636a2816a87b9d
paullaster <paullasterokoth98@gmail.com>
2024-02-23 02:47:42 +0300

    add mpesa for payment

--- src/config/mpesa.js
+++ src/config/mpesa.js
@@ -1,5 +1,7 @@
 export const mpesa = {
     express_test_url: process.env.MPESA_EXPRESS_TEST_URL,
     express_production_url: process.env.MPESA_EXPRESS_PRODUCTION_URL,
-    authorization_url: process.env.MPESA_AUTHORIZATION_URL
+    authorization_url: process.env.MPESA_AUTHORIZATION_URL,
+    consumer_key: process.env.MPESA_CONSUMER_KEY,
+    comsumer_secret: process.env.MPESA_CONSUMER_SECRET
 }
