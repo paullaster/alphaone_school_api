79bf220810dd672a30d2e07c14f2047c68702b00
paullaster <paullasterokoth98@gmail.com>
2024-02-23 18:17:51 +0300

    add member

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ import { usersRouter } from './users/index.js';
 import { useCoursesRoutes } from './courses/index.js';
 import { useApplicationRoutes } from './applications/index.js';
 import { useResourceRoutes } from './resources/index.js';
+import { usePaymentsRoutes } from './payments/index.js';
 
 const router = express.Router();
 
@@ -12,4 +13,5 @@ router.use('/users', usersRouter);
 router.use(useCoursesRoutes);
 router.use(useApplicationRoutes);
 router.use(useResourceRoutes);
+router.use(usePaymentsRoutes);
 export {router};
