d2a9e8427e9b7eee2602fa2fd91397694be53e3e
paullaster <paullasterokoth98@gmail.com>
2024-02-23 04:21:55 +0300

    add mpesa for payment

--- src/app/lib/mpesa/Mpesa.js
+++ src/app/lib/mpesa/Mpesa.js
@@ -56,6 +56,7 @@ async getMpesaToken() {
             const hour = now.getHours();
             const minute = now.getMinutes();
             const second = now.getSeconds();
+            ti
         } catch (error) {
             
         }
