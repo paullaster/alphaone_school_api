ac0b88b8972ba42599dfb21fbd652405e166f2c9
paullaster <paullasterokoth98@gmail.com>
2024-02-23 04:33:53 +0300

    add mpesa for payment

--- src/app/lib/mpesa/Mpesa.js
+++ src/app/lib/mpesa/Mpesa.js
@@ -47,18 +47,22 @@ async getMpesaToken() {
             
         }
     }
-    async timeStamp() {
-        try {
-            const now = new Date();
-            const year = now.getFullYear();
-            const month = (now.getMonth() + 1) < 10 ? '0' + (now.getMonth() + 1) : now.getMonth() + 1;
-            const day = now.getDate() <  10 ? '0' + (now.getDate()) : now.getDate();
-            const hour = now.getHours() < 10 ? '0' + (now.getHours()) : now.getHours();
-            const minute = now.getMinutes() < 10 ? '0' + (now.getMinutes()) : now.getMinutes(); 
-            const second = now.getSeconds() < 10 ? '0' + (now.getSeconds()) : now.getSeconds(); 
-            return `${year}${month}${day}${hour}${minute}${second}`;
-        } catch (error) {
-            return error.message;
-        }
-    }
+    /**
+ * Returns a timestamp in the format YYYYMMDDhhmmss
+ * @returns {string} timestamp
+ */
+async timeStamp() {
+  try {
+    const now = new Date();
+    const year = now.getFullYear();
+    const month = (now.getMonth() + 1) < 10 ? '0' + (now.getMonth() + 1) : now.getMonth() + 1;
+    const day = now.getDate() <  10 ? '0' + (now.getDate()) : now.getDate();
+    const hour = now.getHours() < 10 ? '0' + (now.getHours()) : now.getHours();
+    const minute = now.getMinutes() < 10 ? '0' + (now.getMinutes()) : now.getMinutes(); 
+    const second = now.getSeconds() < 10 ? '0' + (now.getSeconds()) : now.getSeconds(); 
+    return `${year}${month}${day}${hour}${minute}${second}`;
+  } catch (error) {
+    return error.message;
+  }
+}
 }
