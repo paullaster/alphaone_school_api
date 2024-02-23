cea5bddc4cb1f6df5a9413d7ab04c8a6cc11c5f5
paullaster <paullasterokoth98@gmail.com>
2024-02-23 18:07:44 +0300

    add member

--- src/config/mpesa.js
+++ src/config/mpesa.js
@@ -7,5 +7,5 @@ export const mpesa = {
     consumer_secret: process.env.MPESA_CONSUMER_SECRET,
     business_shortcode: process.env.MPESA_BUSSINESS_SHORTCODE,
     mpesa_passkey: process.env.MPESA_PASSKEY,
-    mpesa_callback: `${application.url}/api/payment/mpesa/transaction`,
+    mpesa_callback: `${application.url}/api/payment/mpesa/niPushCallback`,
 }
