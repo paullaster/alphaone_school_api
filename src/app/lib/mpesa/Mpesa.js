4327c698649dfafe03452a99436010e7a2aa34d6
paullaster <paullasterokoth98@gmail.com>
2024-02-23 04:16:31 +0300

    add mpesa for payment

--- src/app/lib/mpesa/Mpesa.js
+++ src/app/lib/mpesa/Mpesa.js
@@ -1,35 +1,50 @@
 import Axios from '../axios/axios.js';
 import { mpesa } from '../../../config/mpesa.js';
 class Mpesa {
-   async getMpesaToken() {
+    /**
+ * Get M-Pesa Token
+ * @returns {string} access_token
+ */
+async getMpesaToken() {
+  try {
+    const joinedKeys = `${mpesa.consumer_key}:${mpesa.consumer_secret}`;
+    const response = Axios._request(mpesa.authorization_url, {
+      headers: {
+        Authorization: `Basic ${new Buffer.from(joinedKeys).toString('base64')}`,
+      },
+      params: {
+        grant_type: 'client_credentials',
+      },
+    });
+    return response.data.access_token;
+  } catch (error) {
+    return error.message;
+  }
+}
+    async niPush(transaction) {
         try {
-            const joinedKeys = `${mpesa.consumer_key}:${mpesa.consumer_secret}`;
-        const response = Axios._request(mpesa.authorization_url, {
-            headers: {
-                Authorization: `Basic ${new Buffer.from(joinedKeys).toString('base64')}`
-            },
-            params: {
-                grant_type: 'client_credentials',
-            }
-        });
-        return response.data.access_token;
+            const token = this.getMpesaToken();
+            const body = {
+
+            };
+            const response = await Axios._request(mpesa.express_api_url, {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                },
+                params: {
+                    shortcode: mpesa.shortcode,
+                }
+            }).catch(err => { });
+            return response.data;
         } catch (error) {
-            return error;
+
         }
     }
-    async niPush(transaction) {
-        const token = await this.getMpesaToken();
-        const body = {
-
-        };
-        const response = await Axios._request(mpesa.express_api_url, {
-            headers: {
-                Authorization: `Bearer ${token}`
-            },
-            params: {
-                shortcode: mpesa.shortcode,
-            }
-        }).catch(err => {});
-        return response.data;
+    async password() {
+        try {
+            
+        } catch (error) {
+            
+        }
     }
 }
