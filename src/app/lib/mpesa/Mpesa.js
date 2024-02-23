e74996fb28af42006a67d98e4ae0a68bb21c6121
paullaster <paullasterokoth98@gmail.com>
2024-02-23 03:53:56 +0300

    add mpesa for payment

--- src/app/lib/mpesa/Mpesa.js
+++ src/app/lib/mpesa/Mpesa.js
@@ -4,13 +4,17 @@ import { mpesa } from '../../../config/mpesa.js';
 class Mpesa {
    async getMpesaToken() {
         const joinedKeys = `${mpesa.consumer_key}:${mpesa.consumer_secret}`;
-        const response = await Axios._request(mpesa.authorization_url, {
+        const response = Axios._request(mpesa.authorization_url, {
             headers: {
                 Authorization: `Basic ${new Buffer.from(joinedKeys).toString('base64')}`
             },
             params: {
                 grant_type: 'client_credentials',
             }
-        })
+        }).catch(err => {});
+        return response.data.access_token;
+    }
+    async niPush() {
+
     }
 }
