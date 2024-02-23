641997eba528e19cfcabe00780de00206e38638f
paullaster <paullasterokoth98@gmail.com>
2024-02-23 04:45:03 +0300

    add mpesa for payment

--- src/config/mpesa.js
+++ src/config/mpesa.js
@@ -7,5 +7,5 @@ export const mpesa = {
     consumer_secret: process.env.MPESA_CONSUMER_SECRET,
     business_shortcode: process.env.MPESA_BUSSINESS_SHORTCODE,
     mpesa_passkey: process.env.MPESA_PASSKEY,
-    mpesa_callback: `${application.url}`
+    mpesa_callback: `${application.url}/api/payment/mpesa/transaction`
 }
