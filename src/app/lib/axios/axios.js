3eb7497d77926520a671a7b455a7722792d6387e
paullaster <paullasterokoth98@gmail.com>
2024-02-23 05:45:50 +0300

    add mpesa for payment

--- src/app/lib/axios/axios.js
+++ src/app/lib/axios/axios.js
@@ -15,35 +15,3 @@ async _request(baseURL, options = {}) {
 }
 }
 export default new Axios();
-
-
-
-// export const _request = axios.create({
-//     baseURL: APIBASEURL,
-// });
-
-
-// const requestInterceptor = (config) => {
-//     // if (AuthService.check()) {
-//         // config.headers.Authorization = `Bearer ${AuthService.Token()}`;
-//         if (!config.headers.Accept && !config.headers['Content-Type'])config.headers['Content-Type'] = "Application/json" 
-//     // }
-//     config.validateStatus = function (status) {
-//         return status >= 200 && status < 300;
-//     };
-//     return config;
-// }
-
-// const errorInterceptor = (error) => {
-// 	console.log("ERROR INTERCEPTOR", error);
-// 	return Promise.reject(error);
-// };
-
-// _request.interceptors.request.use(requestInterceptor, errorInterceptor);
-
-
-// const reponseInterceptor = (response) => {
-// 	return Promise.resolve(response?.data);
-// };
-
-// _request.interceptors.response.use(reponseInterceptor, errorInterceptor);
