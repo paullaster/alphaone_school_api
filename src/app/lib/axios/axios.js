031f24842a22a22bd919b7079a5001785e887b72
paullaster <paullasterokoth98@gmail.com>
2024-02-23 03:28:58 +0300

    add mpesa for payment

--- src/app/lib/axios/axios.js
+++ src/app/lib/axios/axios.js
@@ -1,12 +1,18 @@
 import axios from 'axios';
 
 class Axios {
-    _request(baseURL, options = {}) {
-        return axios.create({
-            baseURL: baseURL,
-            ...options,
-        });
-    }
+    /**
+ * Creates a new instance of Axios with the given configuration.
+ * @param {string} baseURL - The base URL for all requests.
+ * @param {AxiosRequestConfig} [options] - The options for the Axios instance.
+ * @returns {Axios} The new Axios instance.
+ */
+async _request(baseURL, options = {}) {
+    return axios.create({
+        baseURL: baseURL,
+        ...options,
+    });
+}
 }
 export default new Axios();
 
