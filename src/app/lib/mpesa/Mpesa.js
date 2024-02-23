9ea1b95f1516ddef07eb9a5fb2e1ae0cbbb833d2
paullaster <paullasterokoth98@gmail.com>
2024-02-23 04:55:04 +0300

    add mpesa for payment

--- src/app/lib/mpesa/Mpesa.js
+++ src/app/lib/mpesa/Mpesa.js
@@ -33,7 +33,7 @@ async getMpesaToken() {
                 PartyA: this.formatPhoneNumber(transaction.phonumber),
                 PartyB: mpesa.business_shortcode,
                 PhoneNumber: this.formatPhoneNumber(transaction.phonumber),
-               CallBackURL: transaction.CallBackURL,
+               CallBackURL: mpesa.mpesa_callback,
             };
             const response = await Axios._request(mpesa.express_api_url, {
                 headers: {
